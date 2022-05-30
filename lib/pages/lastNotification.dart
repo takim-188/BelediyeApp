52fa10e4a113b8628a96f3521e26c5d17ccc84d5
Beyza Gonculer <beyza.gonculer1@gmail.com>
2022-05-30 11:13:43 +0300

    renkler

--- lib/pages/lastNotification.dart
+++ lib/pages/lastNotification.dart
@@ -12,9 +12,44 @@ class _LastNotificationState extends State<LastNotification> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Geçmiş Bildiriler DetailPage"),
+        backgroundColor: Color(0xFF00D9D2),
+        title: Text("Geçmiş Bildiriler Detay Sayfası"),
       ),
-      backgroundColor: Colors.white,
+      body: FittedBox(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.all(16.0),
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Column(
+            children: const [
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text('GG/AA/YYYY SS:DD'),
+              ),
+              SizedBox(height: 16 ),
+
+              Text(
+                'Bildirim Detayı',
+                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold) ,
+              ),
+              //SizedBox(height: 16.0,)
+              // Image(image: AssetImage(assetName))  // Bildirim detayı resim olacaksa aktif edin
+              SizedBox(height: 16.0),
+              Text(
+                "Bildirim Açıklama Alanı",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
